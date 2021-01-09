826234fd077b8c072f44886dacffbc8d2a0df601
Edrick Da Corte Henriquez <36369935+edrickdch@users.noreply.github.com>
2021-01-09 17:52:29 -0500

    Fix symbolic links in install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ brew install $(cat ./brew/casks)
 
 dotfiles=(.bashrc .gitconfig .gitmessage .tmux.conf .vimrc)
 for file in ${dotfiles[@]}; do
- echo ln -s $HOME/mac-dotfiles/$file $HOME/$file
+ ln -s $HOME/mac-dotfiles/$file $HOME/$file
 done
 
